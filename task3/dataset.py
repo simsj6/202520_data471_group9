47166eb4e7928d716db52d1d91516756d65c16d3
zetami <joshuacrain00@gmail.com>
2025-06-02 16:58:02 -0700

    data parsing progress

--- task3/dataset.py
+++ task3/dataset.py
@@ -3,15 +3,38 @@ import sklearn as sk
 import numpy as np
 
 class dataset:
-    def __init__(this, file):
+    def __init__(this):
         this.file = "./task3_ratings/dev.sparseX"
-        this.features = np.loadtxt(file)
-        this.data = np.(())
+        this.fileConfig = "./task3_ratings/task3.config"
+        this.features = np.loadtxt(this.file)
+        this.data = this.load_data()
 
-    def parser(this, i):
+    def parser(this):
         row = this.features[:, 0]
         col = this.features[:, 1]
         val = this.features[:, 2]
+        return row, col, val
 
-
+    # Load config values
+    def load_config(this, path):
+        with open(path, 'r') as f:
+            token = f.read().split()
+        n_train = int(token[1])
+        n_dev = int(token[3])
+        d = int(token[5])
+        c = int(token[7])
+        return n_train, n_dev, d, c
     
+    def load_data(this):
+        n_train, n_dev, d, c = this.load_config(this.fileConfig)
+        data = np.zeros((n_dev, d))
+        for i in this.features:
+            row, col, val = this.parser()
+            data[row, col] = val
+            
+        return data
+    
+print("Gathering data")
+obj = dataset()
+print(obj.data)
+print("done")
